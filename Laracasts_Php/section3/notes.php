0e2fe640075c546fe7062b3128ea7221e15b830e
AhmedElazony <ahmedazony2@gmail.com>
2023-04-15 18:11:13 +0200

    Editing the notes file

--- Laracasts_Php/section3/notes.php
+++ Laracasts_Php/section3/notes.php
@@ -5,7 +5,7 @@ $heading = "My Notes";
 $config = require('config.php');
 $db = new Database($config['database'], 'root', 'helloworld');
 
-$notes = $db->query("SELECT * FROM notes WHERE user_id = 1")->fetchAll();
+$notes = $db->query("SELECT * FROM notes WHERE user_id = 1")->get();
 
 require 'Views/notes.view.php';
 
@@ -37,6 +37,25 @@ require 'Views/notes.view.php';
 
 <?php // =--------------------------------------------------------------- ?>
 
+/* the note file */
+
+<?php 
+
+$heading = "Notes";
+$currentUserId = 1;
+
+$config = require('config.php');
+$db = new Database($config['database'], 'root', 'helloworld');
+
+$note = $db->query("SELECT * FROM notes WHERE id = :id", ['id' => $_GET['id']])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+require 'Views/note.view.php';
+?>
+
+<?php // =--------------------------------------------------------------- ?>
+
 /* the note view  */
 
 <?php require('Partials/head.php'); ?>
@@ -46,10 +65,10 @@ require 'Views/notes.view.php';
 <?php require('Partials/banner.php'); ?>
 
 <main>
-  <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+<div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
     <a href="/demo/notes" class="text-blue-500 hover:underline">Go Back..</a>
     <p class="mt-6"><?= $note['body']; ?></p>
-  </div>
+</div>
 </main>
 
 <?php require('Partials/footer.php'); ?>
@@ -58,8 +77,10 @@ require 'Views/notes.view.php';
 
 /* Database Class */
 <?php
+
 class Database {
     private $connection;
+    public $statement;
     public function __construct($config, $username = 'root', $password = '')
     {
         // to build the dsn automatically
@@ -72,9 +93,26 @@ class Database {
     }
     public function query($query, $params = [])
     {
-        $statement = $this->connection->prepare($query);
-        $statement->execute($params);
+        $this->statement = $this->connection->prepare($query);
+        $this->statement->execute($params);
 
-        return $statement;
+        return $this;
+    }
+
+    public function find() {
+        return $this->statement->fetch();
+    }
+
+    public function get() {
+        return $this->statement->fetchAll();
+    }
+
+    public function findOrFail() {
+        $result = $this->find();
+
+        if (! $result) {
+            abort(); // a fucntion in the functions file
+        }
+        return $result;
     }
 }
